a24d00dc4b4debc7cdff622cb3ee395668a81946
nishaids <your-nishaids-email@gmail.com>
2026-03-23 02:23:45 +0530

    Fix AdSense crossOrigin

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2372917044233355"
+     crossOrigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
